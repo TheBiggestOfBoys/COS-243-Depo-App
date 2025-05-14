bcbda8704ae805296e7dd13fea42a6facc12509d
TheBiggestOfBoys <jrscott812@outlook.com>
2025-05-14 00:18:27 -0400

    copied Zeke's config file

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,17 @@
 Rails.application.routes.draw do
-  get 'admin' => 'admin#index'
+  get 'admin' =>  'admin#index'
   controller :sessions do
-    get  'login' => :new
+    get 'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
   end
+
   get 'sessions/create'
   get 'sessions/destroy'
-
-
-  # START_HIGLIGHT
-  resources :support_requests, only: %i[ index update ]
-  # END_HIGLIGHT
   resources :users
-  resources :products do
-    get :who_bought, on: :member
-  end
-
-  scope '(:locale)' do
-    resources :orders
-    resources :line_items
-    resources :carts
-    root 'store#index', as: 'store_index', via: :all
-  end
+  resources :orders
+  resources :line_items
+  resources :carts
+  root 'store#index', as: 'store_index'
+  resources :products
 end
